14a603d5aa98d61dac79ba9e8ebd5b5debb77029
Dmytro <xkhoda01@vutbr.cz>
2023-04-22 20:00:56 +0200

    fixed calc_lib again

--- src/Calc_Library.py
+++ src/Calc_Library.py
@@ -61,22 +61,4 @@ def Factorial(n):
     else:
         return n * Factorial(n-1)
 
-def SquareRoot(n):
-    return n ** 0.5
-
-def Power(n, p):
-    return n ** p
-
-def Plus(n, p):
-    return n + p
-
-def Minus(n, p):
-    return n - p
-
-def Multiply(n, p):
-    return n * p
-
-def Divide(n, p):
-    return n / p
-
 
