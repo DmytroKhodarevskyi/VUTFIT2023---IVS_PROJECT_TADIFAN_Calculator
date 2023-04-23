650a1720c87991c34b69074ece9a915599082892
Xhrn00 <xhryny00@vutbr.cz>
2023-04-23 21:04:02 +0200

    setup2

--- src/setup.py
+++ src/setup.py
@@ -4,11 +4,11 @@ setup(
     name="MyProgram",
     version="1.0",
     description="My PyQt5 program",
-    executables=[Executable("main.py")],
+    executables=[Executable("main.py", base="Win32GUI")],
     options={
         "build_exe": {
-            "include_files": ["Calc_Library.py", "Newdesign.py"],
-            "packages": ["PySide6", "decimal", "keyboard", "typing"],
+            "include_files": ["Calc_Library.py", "Newdesign.py", "docs", "icons", "Rubik", "files_rc.py"],
+            "packages": ["PySide6", "keyboard", "typing", "decimal", "importlib", "sys", "os"],
             "include_msvcr": True,
         }
     },
